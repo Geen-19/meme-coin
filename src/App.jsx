ca5cf03ac9d097005966a422e838c864fa7fb1d3
geen <angelinedias53@gmail.com>
2024-12-17 21:07:22 +0530

    homepage corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 import PikachuEyes from "../components/Pikachu/Pickachu";
